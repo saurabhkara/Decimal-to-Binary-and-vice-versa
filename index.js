bcf8b804e27567853acd0a5e863d9f45965c61a5
Saurabh <saurabkumar679@gmail..com>
2022-11-17 13:20:50 +0530

    armstrong number

--- index.js
+++ index.js
@@ -31,3 +31,53 @@ function binaryToDecimal(){
 // binaryToDecimal();
 
 
+function primeNumber(){
+    let num = +prompt('Enter the number');
+    let flag=false;
+    for(let i=2;i<=Math.sqrt(num); i++){
+        if(num % i === 0){
+            flag =true;
+            break;
+        }
+    }
+    if(flag){
+        console.log('Number is non-prime',num);
+    }else{
+        console.log(num,' is Prime number');
+    }
+}
+
+// primeNumber();
+
+function reverseTheNumber(){
+    let num = +prompt('Enter the number');
+    let temp = num;
+    let result=0;
+
+    while(temp){
+        result = (result*10) + (temp%10);
+        temp = parseInt(temp/10);
+        
+    }
+    console.log(result);
+}
+
+// reverseTheNumber();
+
+function armStrongNumber(){
+    let num = +prompt('Enter the number');
+    let temp=num;
+    let result =0;
+    while(temp){
+        result= result + ((temp%10)**3);
+        temp = parseInt(temp /10);
+    }
+    console.log(result,num);
+    if(num === result){
+        console.log(num, ' is armstrong number');
+    }else{
+        console.log(num,' is not armstrong number');
+    }
+
+}
+// armStrongNumber();
